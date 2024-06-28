c5ca4a93aa6dcb6d4b1e498ee6941b635810c8e7
Muhammad Kashif <mkashif.aslam36@gmail.com>
2024-06-28 20:36:32 +0500

    add redis simple and hash key value set get methods

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -5,16 +5,37 @@ import { RedisService } from './redis.service';
 export class RedisController {
   constructor(private readonly redisService: RedisService) {}
 
-  @Get('read')
-  async getData(@Query('key') key: string): Promise<any> {
-    return await this.redisService.getData(key);
+  @Get('simple')
+  readSimple(@Query('key') key: string): Promise<any> {
+    return this.redisService.getSimple(key);
   }
 
-  @Post('write')
-  async createData(
+  @Get('hash-set')
+  getHashSet(@Query('key') key: string): Promise<any> {
+    return this.redisService.getHashSet(key);
+  }
+
+  @Get('hash-key')
+  getHashKey(
+    @Query('set') set: string,
+    @Query('key') key: string,
+  ): Promise<any> {
+    return this.redisService.getHashKey(set, key);
+  }
+
+  @Post('simple')
+  setSimple(
     @Body('key') key: string,
     @Body('value') value: string,
   ): Promise<any> {
-    return await this.redisService.createData(key, value);
+    return this.redisService.setSimple(key, value);
+  }
+
+  @Post('hash')
+  setHash(
+    @Body('key') key: string,
+    @Body('value') value: Record<string, string | number>,
+  ): Promise<any> {
+    return this.redisService.setHashSet(key, value);
   }
 }
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -3,13 +3,35 @@ import { RedisClientType } from 'redis';
 
 @Injectable()
 export class RedisService {
+  private readonly EXPIRY_TIME_60_SECOND = 60;
+
   constructor(@Inject('RedisClient') private client: RedisClientType) {}
 
-  getData(key: string) {
+  getSimple(key: string) {
     return this.client.get(key);
   }
 
-  createData(key: string, value: string) {
-    return this.client.set(key, value);
+  getHashSet(key: string) {
+    return this.client.hGetAll(key);
+  }
+
+  getHashKey(set: string, key: string) {
+    return this.client.hGet(set, key);
+  }
+
+  async setSimple(key: string, value: string) {
+    const result = this.client.set(key, value);
+    await this.setExpiry(key);
+    return result;
+  }
+
+  async setHashSet(key: string, value: Record<string, string | number>) {
+    const result = await this.client.hSet(key, value);
+    await this.setExpiry(key);
+    return result;
+  }
+
+  async setExpiry(key: string, expiry: number = this.EXPIRY_TIME_60_SECOND) {
+    await this.client.expire(key, expiry);
   }
 }
